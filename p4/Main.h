b7c5272ff9d7e62baab565b9c9dd376ce0e810b6
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-05-19 16:02:51 -0700

    Forgot to add the header file

--- p4/Main.h
+++ p4/Main.h
@@ -5,4 +5,27 @@ header Main
   functions
     main ()
 
+  class ResourceMonitor
+   superclass Object
+   fields
+     monitorMutex : MonitorMutex
+     mutex        : Mutex
+     hCondition   : HCondition
+     mCondition   : Condition
+     cvType       : int
+     numResource  : int
+
+   methods
+     Init    (semantic: int)
+     Request (numRequested, thId: int)
+     Release (numReleased, thId: int)
+
+     Lock   ()
+     Unlock ()
+     Wait   ()
+     Signal ()
+
+     Error  ()
+  endClass
+
 endHeader
